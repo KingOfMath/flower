c07f05672e4c0b813d78ed7f5ca70bee90514fb2
TheDaniel97 <gajbhiye.saurabh123@gmail.com>
2021-06-05 19:48:26 +0530

    generating the private key and public key for the server.

--- examples/paillier_homomorphic_encryption/server.py
+++ examples/paillier_homomorphic_encryption/server.py
@@ -2,6 +2,13 @@ import flwr as fl
 from examples.paillier_homomorphic_encryption.paillier_server import PaillerServer
 from flwr.server.client_manager import SimpleClientManager
 
+
+
+
+
+
+
+
 # Start Flower server for three rounds of federated learning
 if __name__ == "__main__":
     fl.server.start_server("localhost:8080", config={"num_rounds": 3},
